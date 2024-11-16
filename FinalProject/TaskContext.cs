765a1ce2a42c392809c1e8b2a548066a372d22bc
Youssef Mohammed <162961577+YoussefJoe2007@users.noreply.github.com>
2024-11-16 13:49:50 +0200

    Update TaskContext.cs

--- FinalProject/TaskContext.cs
+++ FinalProject/TaskContext.cs
@@ -4,6 +4,7 @@ namespace FinalProject
 {
     public class TaskContext : DbContext
     {
+    //user
         public DbSet<User> Users { get; set; }
         public DbSet<Course> Courses { get; set; }
     }
